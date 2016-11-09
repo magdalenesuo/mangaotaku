daf50e801ff42a198e680f078b5b6d6f9e603f06
magdalenesuo <medomag20@gmail.com>
2016-11-09 06:38:02 +0530

    added download link

--- webapp/views.py
+++ webapp/views.py
@@ -5,6 +5,8 @@ import flask
 import mangaScrape
 from webapp import app
 import re
+import shutil
+import os
 
 # importing application wide parameters and global variables that have been
 # defined in __init__.py
@@ -14,16 +16,27 @@ def webapp():
 	url=""
 	pages=""
 	folder=""
+	filename=""
 	if flask.request.method=='POST':
 		url = flask.request.form.get('url')
 		pages = flask.request.form.get('pages')
 		folder=re.split(r"/", url)[3] + "_"+re.split(r"/", url)[4]
 		mangaScrape.main(url, folder, int(pages))
+		os.path.dirname(os.path.abspath(__file__))
+		directory_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../" + folder)
+		shutil.make_archive(folder, 'zip', directory_name)
 	return flask.render_template('index.html', url=url, 
-					pages=pages)
+					pages=pages, filename=folder+".zip")
+
+@app.route('/download/<path:filename>')
+def download(filename):
+	download_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
+	return flask.send_from_directory(directory=download_folder, filename=filename) 
+
 @app.route('/about/')
 def about():
     return flask.render_template('about.html')
+
 @app.route('/contact/')
 def contact():
     return flask.render_template('contact.html', yo="pp")
